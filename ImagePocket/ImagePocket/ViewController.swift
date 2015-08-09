93910676d3e1f90519d0e527085fa95259d95fe0
GSerjo <sedot8@gmail.com>
2015-08-09 14:52:46 +0300

    Added photo permission request

--- ImagePocket/ImagePocket/ViewController.swift
+++ ImagePocket/ImagePocket/ViewController.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initInternal()
         
-        // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = MMDrawerBarButtonItem(target: self, action: "leftBarButtonItemPressed")
+        if(PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.Authorized){
+            runApp()
+        }
+        else{
+            PHPhotoLibrary.requestAuthorization(requestAuthorizationHandler)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func leftBarButtonItemPressed(){
+        
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.centerContainer.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
+    }
+    
+    private func initInternal(){
+        
+        self.title = "ImagePocket"
+        navigationItem.leftBarButtonItem = MMDrawerBarButtonItem(target: self, action: "leftBarButtonItemPressed")
+    }
+    
+    private func requestAuthorizationHandler(status: PHAuthorizationStatus){
+        
+        if(status == PHAuthorizationStatus.Authorized){
+            
+            executeInMainQueue{runApp}
+        }
+        else {
+            
+            let alertController = UIAlertController(title: "Warning", message: "The Photo permission was not authorized. Please enable it in aSettings to continue", preferredStyle: .Alert)
+            let settingsAction = UIAlertAction(title: "Open Settings", style: .Default, handler: {_ in
+                
+                if let appSettings = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.sharedApplication().openURL(appSettings)
+                }
+            })
+            
+            alertController.addAction(settingsAction)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func executeInMainQueue(action: ()->Void){
+        dispatch_async(dispatch_get_main_queue(), action)
+    }
+    
+    private func runApp(){
+        print("runnApp")
+        
     }
 
 }
